5b16e0978bb6c346e81612ad6c1eda5db5c86812
acunamatata82 <yar.oleg1982@mail.ru>
2023-07-20 23:01:35 +0300

    create project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 800px;
+  color: #2c3e50;
+  margin: 0 auto;
+}
+</style>
